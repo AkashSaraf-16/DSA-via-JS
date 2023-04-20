fa22283885d87a8952dfb260bd8c2882885b19e2
Akash Saraf <akashsaraf24@gmail.com>
2023-04-20 07:47:05 +0530

    added BFS

---
+++ BFS.js
@@ -0,0 +1,102 @@
+class Node {
+ constructor(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+ }
+}
+
+class BST {
+ constructor() {
+  this.root = null;
+ }
+
+ insert(value) {
+  const newNode = new Node(value);
+  if (this.root === null) {
+   this.root = newNode;
+   return this;
+  }
+  let temp = this.root;
+  while (true) {
+   if (newNode.value === temp.value) return undefined;
+   if (newNode.value < temp.value) {
+    if (temp.left === null) {
+     temp.left = newNode;
+     return this;
+    }
+    temp = temp.left;
+   } else {
+    if (temp.right === null) {
+     temp.right = newNode;
+     return this;
+    }
+    temp = temp.right;
+   }
+  }
+ }
+
+ contains(value) {
+  if (this.root === null) return false;
+  let temp = this.root;
+  while (temp) {
+   if (value < temp.value) {
+    temp = temp.left;
+   } else if (value > temp.value) {
+    temp = temp.right;
+   } else {
+    return true;
+   }
+  }
+  return false;
+ }
+
+ minValueNode(currentNode) {
+  while (currentNode.left !== null) {
+   currentNode = currentNode.left;
+  }
+  return currentNode;
+ }
+
+ BFS() {
+  const bfs = [];
+  const queue = [];
+  const currentNode = this.root;
+  queue.push(currentNode);
+  while (queue.length != 0) {
+   const front = queue[0];
+   queue.shift();
+   if (front != null) {
+    bfs.push(front.value);
+    if (front.left)
+     queue.push(front.left);
+    if (front.right)
+     queue.push(front.right);
+   }
+  }
+  return bfs;
+ }
+
+}
+
+
+
+let myTree = new BST();
+
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+myTree.insert(27);
+myTree.insert(52);
+myTree.insert(82);
+
+console.log(myTree.BFS());
+
+
+/*
+ EXPECTED OUTPUT:
+ ----------------
+ [ 47, 21, 76, 18, 27, 52, 82 ]
+
+*/
